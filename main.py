61017de5468a1f294c16c816aff721d4be1745c9
BaileyRobison <60158032+BaileyRobison@users.noreply.github.com>
2020-02-29 12:14:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import numpy as np
 
 
 #global variables (temporary)
-J_factor = 1.
-K_factor = 2.
-size = 3
-
+J_factor = 1. #nearest-neighbor term factor
+K_factor = 2. #plaquette term factor
+size = 3 #size of state in 1D
+chain_length = 10 #number of states to generate
+T_start = 1000. #starting temperature
 
 #original hamiltonian
 def hamiltonian(state):
@@ -40,4 +41,46 @@ initial_state[initial_state==0] = -1 #replace 0s with -1s
 print(initial_state)
 print hamiltonian(initial_state)
 
+#STEP 1: generate states with local update
 
+#calculate thermodynamic beta
+k_b = 8.617e-5 #Boltzmann constant in eV/K
+beta = k_b * T_start
+
+#markov chain of generated states
+state_chain = []
+state_chain.append(initial_state)
+
+for n in range(chain_length):
+    state = np.copy(state_chain[n])
+    energy = hamiltonian(state)
+    
+    #loop over sites in state to perform local update    
+    for i in range(size):
+        for j in range(size):
+            accept = False #whether or not flip is accepted
+            state[i,j] *= (-1) #try flipping bit
+            flipped_energy = hamiltonian(state) #energy of flipped state
+            
+            if flipped_energy < energy: #accept if lower energy
+                accept = True
+            else: #if higher energy
+                p_rand = np.random.uniform(0, 1)
+                prob_accept = np.exp(-1*beta*(flipped_energy-energy)) #probability of accepting
+                if p_rand < prob_accept: #accept if lower than probability
+                    accept = True
+            
+            if accept==True: #if flip is accepted
+                energy = flipped_energy #update energy
+            else: #if rejected
+                state[i,j] *= (-1) #flip bit back
+            
+    #add state to chain
+    state_chain.append(state)
+    
+for i in range(len(state_chain)):
+    print(state_chain[i])
+
+
+
+    
