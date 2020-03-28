bd02e569098a0b175bd3bf04d5471e8d1757ba4d
BaileyRobison <60158032+BaileyRobison@users.noreply.github.com>
2020-03-28 14:31:37 -0400

    Add files via upload

--- localupdate.py
+++ localupdate.py
@@ -18,6 +18,28 @@ class LocalUpdater():
         #generate random probability
         p_rand = np.random.uniform(0, 1, size=(size,size))
         
+        for n in range(size**2): #loop N times
+            #randomly select a site
+            i,j = np.random.randint(0, size, size=2)
+        
+            accept = False #whether or not flip is accepted
+            energy = self.hamiltonian(state, i, j)
+            state[i,j] *= (-1) #try flipping bit
+            flipped_energy = self.hamiltonian(state, i, j) #energy of flipped state
+            
+            if flipped_energy < energy: #accept if lower energy
+                accept = True
+            else: #if higher energy
+                prob_accept = np.exp(-1*self.beta*(flipped_energy-energy)) #probability of accepting
+                if p_rand[i,j] < prob_accept: #accept if lower than probability
+                    accept = True
+            
+            if accept==False: #if flip is rejected
+                state[i,j] *= (-1) #flip bit back
+                    
+        return state #return updated state
+        
+        '''
         #loop over sites in state to perform local update    
         for i in range(size):
             for j in range(size):
@@ -39,3 +61,4 @@ class LocalUpdater():
                     state[i,j] *= (-1) #flip bit back
                     
         return state #return updated state
+        '''
